b2157520e22ac36c06ab81c310a0448602a9a45a
i-am-jillian <jillianguerrero12@gmail.com>
2026-03-22 05:46:17 -0400

    pa2

---
+++ PA2Python/Solution.py
@@ -0,0 +1,33 @@
+from collections import deque
+class Solution:
+
+    def __init__(self, start_node, graph):
+        self.start_node = start_node
+        self.graph = graph
+
+    def output_distances(self):
+        """
+        :return: the list of minimum distances from each node to the start node
+        """
+        #find the number of nodes in the graph
+        nodes = len(self.graph)
+        #initialize list of distances with -1
+        distance = [-1] * nodes
+        #setting start node at position 0
+        distance[self.start_node] = 0
+        
+        #adding the start node in the queue
+        queue = deque([self.start_node])
+
+        #traversing the graph by taking the node that im standing on and check its neighbors
+        while queue:
+            current = queue.popleft([self.start_node])
+            for neighbors in self.graph[current]:
+                #checking for nodes that are not visited yet (-1)
+                if distance[neighbors] ==  -1:
+                    #set their distance in distance list
+                    distance[neighbors] = distance[current] + 1
+                    #append the node to the queueu
+                    queue.append([neighbors])
+
+        return distance
